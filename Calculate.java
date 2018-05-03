e5a5d3c4de6732670600da14ac038cc9257bcf72
m4k5 <techm4k5@gmail.com>
2018-05-03 22:37:16 +0300

    user imput

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,24 @@
 public class Calculate {
-	public static void main(String[] arg) {
-		System.out.println("Calculate...");
+
+	private int result;
+
+	public void add(int ... params){
+		for(Integer param : params){
+			this.result += param;
 	}
-}
+     }
+
+
+	
+	public int getResult(){
+		return this.result;
+
+	}
+
+	public void cleanResult(){
+		this.result = 0;
+	}	
+
+
+
+}	
